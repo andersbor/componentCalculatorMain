acf78bc35c0c28b3c2a14656baaf25bdb9e57080
andersbor <anbo@easj.dk>
2017-09-24 21:10:57 +0200

    small main

--- CalculatorUsingComponent/Program.cs
+++ CalculatorUsingComponent/Program.cs
@@ -9,7 +9,7 @@ namespace CalculatorUsingComponent
         // Make a reference to the right place in your file system
         static void Main()
         {
-            Console.WriteLine("4 + 5 = " + Calculator.Add(4, 5));
+            Console.WriteLine("4 + 5 = " + Calculator.Add(4, 5)); 
             Console.WriteLine("4.1 + 5.2 = " + Calculator.Add(4.1, 5.2));
             Console.WriteLine("7 / 2 = " + Calculator.Divide(7, 2));
             Console.WriteLine("7.0 / 2.0 = " + Calculator.Divide(7.0, 2.0));
